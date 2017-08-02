e9a7f691d968c1feab3ed5d8f8cdec2a051f1c49
Venkat Peri <venkatperi@gmail.com>
2017-08-02 16:09:44 -0400

    gulp release task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require( 'gulp' );
-var gulpDocumentation = require('gulp-documentation');
+var gulpDocumentation = require( 'gulp-documentation' );
+require( 'gulp-release-it' )( gulp );
 
-gulp.task('docs', function () {
-  return gulp.src('./lib/**.js')
-    .pipe(gulpDocumentation('md'))
-    .pipe(gulp.dest('doc/'));
-});
-
+gulp.task( 'docs', function() {
+  return gulp.src( './lib/**.js' )
+    .pipe( gulpDocumentation( 'md' ) )
+    .pipe( gulp.dest( 'doc/' ) );
+} );
